f86085af4449563914c33c79ca5b6bff23ec702d
Adam Shaffer <ashaffe@gmail.com>
2022-01-10 21:19:06 -0600

    Add deepEqual statment

---
+++ test/chai-test.js
@@ -0,0 +1,11 @@
+const chai = require('chai')
+const expect = chai.expect
+
+describe('chai test', () => {
+    it('should compare some value', () => {
+        expect(1).to.equal(1)
+    })
+    it('should test some other stuff', () => {
+        expect({name:'foo'}).to.deep.equal({name: 'foo'})
+    })
+})
